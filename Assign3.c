7e909ce38a42e977f8ce7497cb575b32a110865c
Pratik <mrpratikonline@gmail.com>
2024-12-28 13:23:56 +0530

    C Program to Add Two Integers

---
+++ Assign3.c
@@ -0,0 +1,13 @@
+// C Program to Add Two Integers
+
+#include <stdio.h>
+
+void main(){
+    int num1,num2,result;
+    printf("Enter Number 1: ");
+    scanf("%d",&num1);
+    printf("Enter Number 2: ");
+    scanf("%d",&num2);
+    result = num1+num2;
+    printf("Addition is %d",result);
+}
