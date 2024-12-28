b4ecfb1d9dcf8b8459f296000246a98e62e10add
Pratik <mrpratikonline@gmail.com>
2024-12-28 13:32:39 +0530

    C Program to Multiply Two Floating-Point Numbers

---
+++ Assign4.c
@@ -0,0 +1,14 @@
+// C Program to Multiply Two Floating-Point Numbers
+
+#include <stdio.h>
+void main(){
+    float num1,num2,result;
+    printf("Mutiplication Program to print two float numbers\n\n");
+    printf("Enter Number 1: ");
+    scanf("%f",&num1);
+    printf("Enter Number 2: ");
+    scanf("%f",&num2);
+    result = num1+num2;
+    printf("Multipication is %f",result);
+
+}
